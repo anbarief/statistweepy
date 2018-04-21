525d39c47aeb13ad194d89a912468217be818ff8
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2018-04-21 18:18:50 +0700

    Create setup.py

---
+++ statistweepy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='statistweepy',
+      version='0.1',
+      description='A simple tool that use tweepy to collect and use Twitter data.',
+      url='http://github.com/anbarief/statistweepy',
+      author='Arief Anbiya',
+      author_email='anbarief@live.com',
+      license='MIT',
+      packages=['statistweepy'],
+      zip_safe=False)
