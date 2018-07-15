327355cdfe2726df06f3b75d4b2cef04cb3abed4
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2018-07-16 02:38:02 +0700

    Update authors_barplots.py

--- examples/authors_barplots.py
+++ examples/authors_barplots.py
@@ -1,16 +1,14 @@
 import numpy
-from statistweepy import collection
 from statistweepy import models
 import matplotlib.pyplot as plt
 
-stats = numpy.load('/home/asus/statistweepy/testfile.npy')
+stats = numpy.load('testfile.npy')
 
 Authors = models.Authors(stats, filter_unique = True)
 
 fig, ax = plt.subplots(1, 1)
-Authors.hbar_plot(ax, measurement = 'Followers', incolor_measurement = 'Following')
-plt.show()
+Authors.hbar_plot(ax, measurement = 'Followers', incolor_measurement = 'Following', textsize = 8, width = 0.5, color = (1,0,0,1))
 
 fig, ax = plt.subplots(1, 1)
-Authors.hbar_plot(ax, measurement = 'Sample Tweets', incolor_measurement = 'Following')
+Authors.hbar_plot(ax, measurement = 'Sample Tweets', incolor_measurement = 'Followers', textsize = 8, width = 0.5, color = (1,0,0,1))
 plt.show()
