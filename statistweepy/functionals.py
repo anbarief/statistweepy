504fd1cb549e1b5831e237c039ead34dca104989
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2018-07-16 01:37:22 +0700

    Update functionals.py

--- statistweepy/functionals.py
+++ statistweepy/functionals.py
@@ -22,14 +22,19 @@ def filter_unique(tweet_stats_list, output = 'status'):
     return unique
 
 def split_texts(texts, adjustment = None):
+    
         split_list = []
+        
         for text in texts:
             split = text.split()
             split_list.extend(split)
+        
         return split_list
 
 def total_rts(tweet_stats_list, string_inclusion = False):
+    
     result = 0
+    
     if not string_inclusion :
         result = sum([tweet.retweet_count for tweet in tweet_stats_list]);
     else:
@@ -37,4 +42,5 @@ def total_rts(tweet_stats_list, string_inclusion = False):
             result = sum([tweet.retweet_count for tweet in tweet_stats_list if string_inclusion in tweet.full_text.split()])
         except:
             result = sum([tweet.retweet_count for tweet in tweet_stats_list if string_inclusion in tweet.text.split()])
+    
     return result
