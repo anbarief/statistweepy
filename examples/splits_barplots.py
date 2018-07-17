f1e37ce7c2da3dce7c16a86a68d6f531a84bba26
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2018-07-17 20:37:12 +0700

    Add files via upload

--- examples/splits_barplots.py
+++ examples/splits_barplots.py
@@ -1,26 +1,26 @@
 import numpy
-from statistweepy import models
+from statistweepy.models import Splits
+from statistweepy.models import Tweets
+from statistweepy.adjustment import Adjustment
 import matplotlib.pyplot as plt
-import statistweepy.adjustment as adjust
-import statistweepy.functionals as func
 
-stats = numpy.load('testfile.npy')
+stats = numpy.load('/home/asus/statistweepy/testfile.npy')
+tweets = Tweets(stats)
 
-Splits = models.Splits(stats, filter_unique = True)
+Model = Splits(stats)
 
-Adjust = adjust.Adjustment(freq_lim = (5, 199), exclude = ('di', 'ke', 'dari'), char_lim = (3, 11111));
-
-fig, ax = plt.subplots(1, 1)
-Splits.hbar_plot(ax, title = '', adjustment = Adjust, color = 'orange')
-plt.show()
+Adjust = Adjustment(freq_lim = (5, 199), exclude = ('the', 'for'), char_lim = (3, 11111));
 
 fig, ax = plt.subplots(1, 1)
-Splits.rbar_plot(ax, title = '', adjustment = Adjust, color  = 'magenta', bar_width = 6, textsize = 10, base_radius = 70)
-plt.show()
+Model.hbar_plot(ax, adjustment = Adjust, height = 1, color = 'orange')
 
 fig, ax = plt.subplots(1, 1)
-Splits.hbar_plot(ax, title = '', adjustment = Adjust, color = (0,0,1,1), incolor_rt = True)
+Model.rbar_plot(ax, adjustment = Adjust, color  = 'orange', bar_width = 6, textsize = 10, base_radius = 70)
 
 fig, ax = plt.subplots(1, 1)
-Splits.rbar_plot(ax, title = '', adjustment = Adjust, color  = (0,0,1,1), incolor_rt = True, bar_width = 6, textsize = 10, base_radius = 70)
+Model.hbar_plot(ax, adjustment = Adjust, height = 0.5, color = 'pink', incolor_rt = True)
+
+fig, ax = plt.subplots(1, 1)
+Model.rbar_plot(ax, adjustment = Adjust, color  = 'pink', incolor_rt = True, bar_width = 6, textsize = 10, base_radius = 70)
+
 plt.show()
