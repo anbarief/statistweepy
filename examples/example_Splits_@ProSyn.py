712e4318d18540e7fd739c5e7899f0a61f95fabe
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2018-08-18 01:28:30 +0700

    Update example_Splits_@ProSyn.py

--- examples/example_Splits_@ProSyn.py
+++ examples/example_Splits_@ProSyn.py
@@ -15,19 +15,17 @@ model = models.Splits(tweets, naive = False)
 #Excluded words
 exc_splits = ('the', 'if', 'The', 'is', 'on', 'are', 'we', 'on', 'at', 'not', 'of', 'to', \
               'than', 'and', 'for', 'that', 'be', 'it', 'in', 'or', 'as')
-#Create an adjustment : only include words that appeared between 80 to 10000 times,
-# and each the length of each word must be between 2 to 100.
-adjustment = models.Adjustment(freq_lim = (80, 10000), char_lim = (2, 100), exclude = exc_splits)
-
-#Visualize
-fig, ax = plt.subplots(1, 1)
-
-#Horizontal bar chart
-model.hbar_plot(ax, adjustment = adjustment, color = 'blue', incolor_rt  = True)
+#Create an adjustment : only include words that appeared between 50 to 10000 times,
+# and each the length of each word must be between 3 to 100.
+adjustment = models.Adjustment(freq_lim = (50, 10000), char_lim = (3, 100), exclude = exc_splits)
 
 fig, ax = plt.subplots(1, 1)
+model.hbar_plot(ax, adjustment = adjustment, color = 'blue', incolor  = 'likes', text_sizes = [12, 15])
 
-#Radial bar chart
-model.rbar_plot(ax, adjustment = adjustment, color = 'blue', bar_width = 3, base_radius = 100)
+fig, ax = plt.subplots(1, 1)
+model.hbar_plot(ax, adjustment = adjustment, color = 'blue', incolor  = 'retweets', text_sizes = [12, 15])
+
+fig, ax = plt.subplots(1, 1)
+model.rbar_plot(ax, adjustment = adjustment, color = (1, 0, 0, 1), bar_width = 3, base_radius = 100, text_sizes = [12, 15])
 
 plt.show(block = False)
